d67a696c6c4a56af5684764c7cb0fc2fc75ef3e1
roxanatic <37555036+roxanatic@users.noreply.github.com>
2018-03-19 21:52:54 +0200

    ImageProcessingProject
    Edge Detection

---
+++ ImageProcessingSystem/Form1.Designer.cs
@@ -0,0 +1,345 @@
+﻿namespace ImageProcessingSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grayImageProcessingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.invertToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.binarizationthresholdingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.laplaceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.RadioButton1 = new System.Windows.Forms.RadioButton();
+            this.RadioButton2 = new System.Windows.Forms.RadioButton();
+            this.RadioButton3 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(11, 73);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(210, 113);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(237, 73);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(210, 113);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.grayImageProcessingToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(878, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openImageToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openImageToolStripMenuItem
+            // 
+            this.openImageToolStripMenuItem.Name = "openImageToolStripMenuItem";
+            this.openImageToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.openImageToolStripMenuItem.Text = "Open Image";
+            this.openImageToolStripMenuItem.Click += new System.EventHandler(this.openImageToolStripMenuItem_Click);
+            // 
+            // grayImageProcessingToolStripMenuItem
+            // 
+            this.grayImageProcessingToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.invertToolStripMenuItem,
+            this.binarizationthresholdingToolStripMenuItem,
+            this.laplaceToolStripMenuItem});
+            this.grayImageProcessingToolStripMenuItem.Name = "grayImageProcessingToolStripMenuItem";
+            this.grayImageProcessingToolStripMenuItem.Size = new System.Drawing.Size(139, 20);
+            this.grayImageProcessingToolStripMenuItem.Text = "Gray Image Processing";
+            this.grayImageProcessingToolStripMenuItem.Click += new System.EventHandler(this.grayImageProcessingToolStripMenuItem_Click);
+            // 
+            // invertToolStripMenuItem
+            // 
+            this.invertToolStripMenuItem.Name = "invertToolStripMenuItem";
+            this.invertToolStripMenuItem.Size = new System.Drawing.Size(217, 22);
+            this.invertToolStripMenuItem.Text = "Invert";
+            this.invertToolStripMenuItem.Click += new System.EventHandler(this.invertToolStripMenuItem_Click);
+            // 
+            // binarizationthresholdingToolStripMenuItem
+            // 
+            this.binarizationthresholdingToolStripMenuItem.Name = "binarizationthresholdingToolStripMenuItem";
+            this.binarizationthresholdingToolStripMenuItem.Size = new System.Drawing.Size(217, 22);
+            this.binarizationthresholdingToolStripMenuItem.Text = "Binarization  (thresholding)";
+            this.binarizationthresholdingToolStripMenuItem.Click += new System.EventHandler(this.binarizationthresholdingToolStripMenuItem_Click);
+            // 
+            // laplaceToolStripMenuItem
+            // 
+            this.laplaceToolStripMenuItem.Name = "laplaceToolStripMenuItem";
+            this.laplaceToolStripMenuItem.Size = new System.Drawing.Size(217, 22);
+            this.laplaceToolStripMenuItem.Text = "Laplace";
+            this.laplaceToolStripMenuItem.Click += new System.EventHandler(this.laplaceToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "\"Image files |*.jpg; *.jpeg; *.bmp; *.png; *tif; *tiff; *gif\";";
+            this.openFileDialog1.Title = "Select an image";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(202, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Prag Binarizare:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(289, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(52, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "128";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(358, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Binarizare";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(64, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label3";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(478, 40);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 13);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "Mask Size";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(668, 37);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 13);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "Scale Factor";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(603, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Sigma";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(671, 53);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(50, 20);
+            this.textBox3.TabIndex = 20;
+            this.textBox3.Text = "1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(606, 53);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(50, 20);
+            this.textBox2.TabIndex = 19;
+            this.textBox2.Text = "2";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(741, 50);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Laplacian";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // RadioButton1
+            // 
+            this.RadioButton1.Location = new System.Drawing.Point(539, 40);
+            this.RadioButton1.Name = "RadioButton1";
+            this.RadioButton1.Size = new System.Drawing.Size(42, 17);
+            this.RadioButton1.TabIndex = 16;
+            this.RadioButton1.TabStop = true;
+            this.RadioButton1.Text = "3x3";
+            this.RadioButton1.UseVisualStyleBackColor = true;
+            this.RadioButton1.CheckedChanged += new System.EventHandler(this.RadioButton1_CheckedChanged);
+            // 
+            // RadioButton2
+            // 
+            this.RadioButton2.Location = new System.Drawing.Point(539, 63);
+            this.RadioButton2.Name = "RadioButton2";
+            this.RadioButton2.Size = new System.Drawing.Size(42, 17);
+            this.RadioButton2.TabIndex = 17;
+            this.RadioButton2.TabStop = true;
+            this.RadioButton2.Text = "5x5";
+            this.RadioButton2.UseVisualStyleBackColor = true;
+            this.RadioButton2.CheckedChanged += new System.EventHandler(this.RadioButton2_CheckedChanged);
+            // 
+            // RadioButton3
+            // 
+            this.RadioButton3.Location = new System.Drawing.Point(539, 86);
+            this.RadioButton3.Name = "RadioButton3";
+            this.RadioButton3.Size = new System.Drawing.Size(42, 17);
+            this.RadioButton3.TabIndex = 18;
+            this.RadioButton3.TabStop = true;
+            this.RadioButton3.Text = "7x7";
+            this.RadioButton3.UseVisualStyleBackColor = true;
+            this.RadioButton3.CheckedChanged += new System.EventHandler(this.RadioButton3_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(878, 412);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.RadioButton1);
+            this.Controls.Add(this.RadioButton2);
+            this.Controls.Add(this.RadioButton3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openImageToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem grayImageProcessingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem invertToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem binarizationthresholdingToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton RadioButton1;
+        private System.Windows.Forms.RadioButton RadioButton2;
+        private System.Windows.Forms.RadioButton RadioButton3;
+        private System.Windows.Forms.ToolStripMenuItem laplaceToolStripMenuItem;
+    }
+}
+
---
+++ ImageProcessingSystem/Form1.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+
+namespace ImageProcessingSystem
+{
+    public partial class Form1 : Form
+    {
+        int maskSize = 3;
+        Image<Gray, Byte> imgGray;
+
+        public Form1()
+        {
+            InitializeComponent();
+            clearForm();
+        }
+
+        private void clearForm()
+        {
+            label1.Visible = false; label2.Visible = false; label3.Visible = false; label4.Visible = false; label5.Visible = false; label6.Visible = false;
+            textBox1.Visible = false; textBox2.Visible = false; textBox3.Visible = false;
+            button1.Visible = false; button2.Visible = false;
+            RadioButton1.Visible = false; RadioButton2.Visible = false; RadioButton3.Visible = false;
+            pictureBox2.Visible = false;
+          
+        }
+
+        private void openImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // deschidere imagine 
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = openFileDialog1.FileName.ToString();
+
+                Image<Bgr, Byte> imgRGB = new Image<Bgr, Byte>(fileName);
+                imgGray = imgRGB.Convert<Gray, Byte>();
+
+                pictureBox1.Size = imgGray.Size;
+                pictureBox1.Image = imgGray.ToBitmap();
+
+                CvInvoke.Imshow("Image", imgRGB);
+                // CvInvoke.WaitKey(0);
+            }
+
+        }
+
+        private void invertToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Matrix<float> lutInvert = new Matrix<float>(256,1);
+
+            for (int i = 0; i < lutInvert.Height; i++)
+                lutInvert.Data[i,0] = 255 - i;
+
+            Image<Gray, Byte> imgGrayInvert = new Image<Gray, Byte>(imgGray.Size);
+
+            for (int i = 0; i < imgGray.Height; i++)
+                for (int j = 0; j < imgGray.Width; j++)
+                    imgGrayInvert.Data[i, j, 0] = (Byte)(lutInvert.Data[imgGray.Data[i,j,0], 0]);
+
+            pictureBox2.Left = pictureBox1.Left + pictureBox1.Width + 10;
+            pictureBox2.Size = imgGrayInvert.Size;
+            pictureBox2.Image = imgGrayInvert.ToBitmap();
+            pictureBox2.Visible = true;
+
+        }
+
+        private void binarizationthresholdingToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            label1.Visible = true; 
+            textBox1.Visible = true;
+            button1.Visible = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // binarizare
+            int pragBinTextBox = Convert.ToByte(textBox1.Text);
+            pictureBox2.Left = pictureBox1.Left + pictureBox1.Width + 10;
+            pictureBox2.Size = imgGray.Size;
+            Image<Gray, Byte> imgBinPrag = BinarizareImagine(imgGray, pragBinTextBox);
+            pictureBox2.Image = imgBinPrag.ToBitmap();
+            pictureBox2.Visible = true;
+        }
+
+        private Image<Gray, Byte> BinarizareImagine(Image<Gray, Byte> ImgOrig, int pragBin)
+        {
+            Matrix<float> lutInvert = new Matrix<float>(256, 1);
+
+            for (int i = 0; i < lutInvert.Height; i++)
+                if (i< pragBin)
+                    lutInvert.Data[i, 0] = 0;
+                else
+                    lutInvert.Data[i, 0] = 255;
+
+            Image<Gray, Byte> imgBinFct = new Image<Gray, Byte>(ImgOrig.Size);
+
+            for (int i = 0; i < ImgOrig.Height; i++)
+                for (int j = 0; j < ImgOrig.Width; j++)
+                    imgBinFct.Data[i, j, 0] = (Byte)(lutInvert.Data[ImgOrig.Data[i, j, 0], 0]);
+
+            return imgBinFct;
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int x = e.X;
+            int y = e.Y;
+
+            label2.Text = Convert.ToString(x);
+            label3.Text = Convert.ToString(y);
+            label2.Visible = true; label3.Visible = true;
+        }
+
+        private void grayImageProcessingToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            clearForm();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //mask 3x3
+        private void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            maskSize = 3;
+        }
+        //mask 5x5
+        private void RadioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            maskSize = 5;
+        }
+        //mask 7x7
+        private void RadioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            maskSize = 7;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double sigma = Convert.ToDouble(textBox2.Text);
+            int scaleFactor = Convert.ToInt32(textBox3.Text);
+            pictureBox2.Size = imgGray.Size;
+            pictureBox2.Visible = true;
+
+            double[,] GX= new double[0,0];
+
+            if (maskSize == 3)
+            {
+
+                GX = new double[3,3]
+                {
+                   { 0, -1, 0},
+                   { -1, 4, -1 },
+                   { 0, -1, 0 }
+                    
+                };
+
+
+            }
+            else if (maskSize == 5)
+            {
+                GX = new double[5,5]
+                {
+                    { 0, 0, 1, 0, 0 },
+                    { 0, 1, 2, 1, 0 },
+                    { 1, 2, -16, 2, 1},
+                    { 0, 1, 2, 1, 0 },
+                    { 0, 0, 1, 0, 0 }
+                };
+
+            }
+            else if (maskSize == 7)
+            {
+
+                GX = new double[7,7]
+                {
+                    { 0, 0, 1, 1, 1, 0, 0 },
+                    { 0, 1, 3, 3, 3, 1, 0 },
+                    { 1, 3, 0, -7, 0, 3, 1 },
+                    { 1, 3, -7, -24, -7, 3, 1},
+                    { 1, 3, 0, -7, 0, 3, 1 },
+                    { 0, 1, 3, 3, 3, 1, 0 },
+                    { 0, 0, 1, 1, 1, 0, 0 }
+                };
+
+            }
+            // LoG
+            Matrix<double> log = new Matrix<double>(maskSize, maskSize);
+            int vOS = (maskSize - 1) / 2;
+            for (int i = -vOS; i < vOS; i++)
+                for (int j = -vOS; j < vOS; j++)
+                    log.Data[i + vOS, j + vOS] = scaleFactor*-(1 / (3.14 * Math.Pow(sigma, 4)) * (1 - ((i * i + j * j) / 2 * sigma * sigma)) * Math.Exp(-((i * i + j * j) / (2 * sigma * sigma))));
+            //(double)scaleFactor * (1/(2*3.14*sigma*sigma))*Math.Exp(-(i*i+j*j)/(2*sigma*sigma)); 
+
+            Image<Gray, double> imgL = new Image<Gray, double>(imgGray.Size);
+
+
+            // convolutia
+            for (int i = vOS; i < imgGray.Height - vOS; i++)
+                for (int j = vOS; j < imgGray.Width - vOS; j++)
+                {
+                    imgL.Data[i, j,0] = 0;
+
+                    for (int ii = -vOS; ii <= vOS; ii++)
+                        for (int jj = -vOS; jj <= vOS; jj++)
+                        {
+                            imgL.Data[i, j, 0] += GX[ii + vOS, jj + vOS] * imgGray.Data[i + ii, j + jj, 0];
+                        }
+
+                }
+            pictureBox2.Image = imgL.ToBitmap();
+            pictureBox2.Left = pictureBox1.Left + pictureBox1.Width + 10;
+
+
+
+        }
+
+        private void laplaceToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            button2.Visible = true;
+            RadioButton1.Visible = true;
+            RadioButton2.Visible = true;
+            RadioButton3.Visible = true;
+            label4.Visible = true;
+            label5.Visible = true;
+            label6.Visible = true;
+            textBox2.Visible = true;
+            textBox3.Visible = true;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+    
+}
